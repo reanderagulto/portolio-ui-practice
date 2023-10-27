1028258136869c4e2e96bf5a34741ad89feabede
Reander Agulto <reanderagulto29@gmail.com>
2023-10-28 00:20:55 +0800

    Added data-content.js for Works and Blogs list

---
+++ src/components/ListItem/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const ListItem = (props) => {
+  return (
+    <div className="list-item--container">
+      {props.image && 
+        <div className="list-item--featured-image">
+          <img src={props.image} alt={props.title} />
+        </div>
+      }
+      <div className="list-item--content">
+        <h3 className="list-item--title">
+          {props.title}
+        </h3>
+        <div className="list-item--subtitle">
+          {props.year && <span class="list-item--date-circular">{props.year}</span>}
+          {props.fullDate && <span className="list-item--date-normal">{props.fullDate}</span>}
+          <div className="list-item--categories">
+            {props.categories && props.categories.map((item, index) => {
+              return (
+                <span 
+                  key={index} 
+                  className="list-item--category"
+                >
+                  {item}
+                </span>
+              )
+            })}
+          </div>
+        </div>
+        <p className="list-item--content-snippet">
+          {props.snippet}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
